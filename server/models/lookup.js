105d70b57da0b5302165d98be750453690b366a5
henkvantol <henkjohanvantol@gmail.com>
2018-03-27 13:30:16 +1300

    a

--- server/models/lookup.js
+++ server/models/lookup.js
@@ -1,15 +1,5 @@
 const db = require('../db');
 
-const config = {
-    user: 'sa',
-    password: 'Kc1p1tlum',
-    server: '192.168.25.181', 
-    database: 'MultiPick',
-    // options: {
-    //     encrypt: true // Use this if you're on Windows Azure
-    // }
-}
-
  function getAll() {
      return new Promise(function (resolve, reject) {
          db.get().request().query('select * from dbo.hierarchyType')
